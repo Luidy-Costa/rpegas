40e970aa1112a1cfc93b1b815a6e7f0013e4382e
Luidy Costa <lluidycosta2024@gmail.com>
2026-04-05 00:00:01 -0300

    feat: adicionar cards do model de vida
    adicionando componente de card que sera usado no model de incremento e decremento de vida junto de todos os componentes que estão nesse card indivudualmente para mantes a atomicidade  dos componestes, mas tudo ainda em um estado monolitico no app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,85 @@
-function App() {
+function Botao(props) {
+  const tamanhoBotao = props.tamanhobotao || "w-fit";
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl text-brand font-bold">RPEGAS VTT Iniciado com Tailwind v4!</h1>
-    </div>
-  )
+    <button
+      className={`${props.corfundobotao} ${tamanhoBotao} text-title rounded text-subtitulo py-2 px-4`}
+    >
+      {props.children}
+    </button>
+  );
 }
 
-export default App
+function Input(props) {
+  return (
+    <input
+      type="number"
+      placeholder="0"
+      className={`w-full border-2 ${props.corbordainput} bg-surface text-title  rounded py-2 px-4 outline-none`}
+    />
+  );
+}
+
+function Card(props) {
+  return (
+    <div
+      className={`flex flex-col rounded-2xl gap-4 p-10 ${props.corbordacard} ${props.corfundocard} border-2`}
+    >
+      <h3 className="text-title text-subtitulo">{props.titulocard}</h3>
+
+      <Input corbordainput={props.corbordainput} />
+
+      <div className="flex flex-wrap gap-4">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="p-10 gap-4 bg-primary flex flex-col">
+      <Card corbordacard="border-success" corfundocard="bg-green-100" corbordainput="border-success" titulocard="Aumentar">
+        <Botao corfundobotao="bg-success">
+          +1
+        </Botao>
+        <Botao corfundobotao="bg-success">
+          +5
+        </Botao>
+        <Botao corfundobotao="bg-success">
+          +10
+        </Botao>
+        <Botao corfundobotao="bg-success">
+          +50
+        </Botao>
+        <Botao corfundobotao="bg-success">
+          +100
+        </Botao>
+      </Card>
+      
+      <Card corfundocard="bg-red-100" corbordacard="border-danger" corbordainput="border-danger" titulocard="Diminuir">      
+        <Botao corfundobotao="bg-danger">
+          -1
+        </Botao>
+        <Botao corfundobotao="bg-danger">
+          -5
+        </Botao>
+        <Botao corfundobotao="bg-danger">
+          -10
+        </Botao>
+        <Botao corfundobotao="bg-danger">
+          -50
+        </Botao>
+        <Botao corfundobotao="bg-danger">
+          -100
+        </Botao>
+
+      </Card>
+
+      <Botao corfundobotao="bg-brand" tamanhobotao="w-full">        
+        Confirmar alterações
+      </Botao>
+    </div>
+  );
+}
+
+export default App;
